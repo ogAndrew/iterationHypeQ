d5b92ece7603f2d5306340398f180ef223f1bac2
Braddon Murphy <BraddonMurphy@gmail.com>
2022-01-15 11:57:28 -0500

    first commit

---
+++ client/App.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const App = () => (
+    <div>
+        <h1>Lets get HYPED</h1>
+    </div>
+);
+
+export default App;
---
+++ webpack.config.js
@@ -0,0 +1,65 @@
+const webpack = require('webpack');
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+//  The production mode
+
+module.exports = {
+    // entry point of our app
+    entry: './client/index.js',
+    output: {
+        path: path.resolve(__dirname, 'build'),
+        // publicPath: 'build',
+        filename: 'bundle.js',
+    },
+    mode: process.env.NODE_ENV,
+    module: {
+        rules: [
+            {
+                test: /\.jsx?/,  // conditional  '.js(x)'
+                exclude: /node_modules/,
+                loader: 'babel-loader', // if it meets a conditional run it..
+                options: {
+                    presets: ['@babel/env', '@babel/react'],
+                }
+            },
+            {
+                test: /\.(css|scss)$/,
+                exclude: /node_modules/,
+                use: [
+                    // Creates `style` nodes from JS strings
+                    "style-loader",
+                    // Translates CSS into CommonJS
+                    "css-loader",
+                    // Compiles Sass to CSS
+                    "sass-loader",
+                ]
+            },
+            {
+                test: /\.png|svg|jpg|gif$/,
+                use: ["file-loader"],
+            }
+        ]
+    },
+    plugins: [new HtmlWebpackPlugin({ template: "index.html" })],
+    devServer: {
+        // static: {
+        //     directory: path.join(__dirname, 'build'),
+        //     publicPath: "/build",
+        // },
+        // compress: true,
+        port: 8080,
+        proxy: {
+            '/api/leaders': 'http://localhost:3000',
+        },
+    },
+    performance: {
+        hints: false,
+        maxEntrypointSize: 512000,
+        maxAssetSize: 512000
+    },
+    resolve: {
+        modules: [__dirname, "client", "node_modules"],
+        extensions: ["*", ".js", ".jsx", ".tsx", ".ts"],
+    },
+};
